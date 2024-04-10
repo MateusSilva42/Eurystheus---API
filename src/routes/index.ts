75445ff8d5b0a3d4d4f396441aa27dfca3e0e42a
mateus silva <mateus42silva@gmail.com>
2024-04-10 15:53:19 -0300

    add authenticate middleware in routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,12 @@ import { userRouter } from "./user";
 import { taskRouter } from "./task";
 import { authRouter } from "./auth";
 import { Router } from "express";
+import { isUser } from "../middlewares/authenticate";
 
 const router = Router();
 
-router.use("/user", userRouter());
-router.use("/task", taskRouter());
+router.use("/user", isUser, userRouter());
+router.use("/task", isUser, taskRouter());
 router.use("/auth", authRouter());
 
 export default { router };
